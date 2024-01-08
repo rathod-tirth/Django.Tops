81e639d2a48d90f9d111ff9c37e9e47e29ac9075
rathod-tirth <tirthrathod6@gmail.com>
2024-01-08 19:38:37 +0530

    add validation and redirect of login page to index

--- Digital_Society/myproject/myapp/views.py
+++ Digital_Society/myproject/myapp/views.py
@@ -27,20 +27,30 @@ def home(request):
 #       return render(request, "myapp/login.html")
 
 def login(request):
-   msg=False
-   try:
-      print("=========>>> Login")
-      user_email=request.POST.get('email')
-      user_password=request.POST.get('password')
-      print("=========>>>",user_email,user_password)
-      
-      if(user_email != None and user_password != None):
-         user_data=User.objects.get(email=user_email, password=user_password)
-         print("=========>>>",user_data)
-      
-   except Exception as e:
-      print("===========>>> Error =",e)
-      msg="Invalid Input Pls Enter Again"
-      
-      
-   return render(request, "myapp/login.html", {'msg':msg})
+   if 'email' in request.session:
+      return render(request, 'myapp/index.html')
+   else:
+      msg=False
+      try:
+         # user input
+         print("=========>>> Login")
+         user_email=request.POST.get('email')
+         user_password=request.POST.get('password')
+         print("=========>>>",user_email,user_password)
+         
+         if(user_email != None and user_password != None):
+            # user object/row
+            user_data=User.objects.get(email=user_email, password=user_password)
+            chairman_data=Chairman.objects.get(userid=user_data.id)
+            print("=========>>>",user_data,chairman_data)
+            
+            request.session['email']=user_data.email
+            
+            return render(request, 'myapp/index.html')
+         
+      except Exception as e:
+         print("===========>>> Error =",e)
+         msg="Invalid Input Pls Enter Again"
+         
+         
+      return render(request, "myapp/login.html", {'msg':msg})
