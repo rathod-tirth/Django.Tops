5df103479389856a7220df40b4f820840487f385
rathod-tirth <tirthrathod6@gmail.com>
2024-01-09 00:36:49 +0530

    add DEBUG code for media files

--- Digital_Society/myproject/myapp/urls.py
+++ Digital_Society/myproject/myapp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('logout/', views.logout, name="logout"),
     path('profile/', views.profile, name="profile")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
