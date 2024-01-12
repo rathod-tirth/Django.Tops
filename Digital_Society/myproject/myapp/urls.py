da261eeec653586bf504a104343dc9099d6be3da
rathod-tirth <tirthrathod6@gmail.com>
2024-01-12 23:25:31 +0530

    add addmember page

--- Digital_Society/myproject/myapp/urls.py
+++ Digital_Society/myproject/myapp/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name="logout"),
-    path('profile/', views.profile, name="profile")
+    path('profile/', views.profile, name="profile"),
+    path('new_pass/', views.new_pass, name="new_pass"),
 ]
 
 if settings.DEBUG:
