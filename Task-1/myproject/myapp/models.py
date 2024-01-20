aaba14bb52db49217024955f93fac91ed4adcc37
rathod-tirth <tirthrathod6@gmail.com>
2024-01-20 19:40:22 +0530

    add User model

--- Task-1/myproject/myapp/models.py
+++ Task-1/myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+   name=models.CharField(max_length=50)
+   subject=models.TextField()
+   gender=models.CharField(max_length=10)
+   
+   def __str__(self):
+       return self.name
+   
