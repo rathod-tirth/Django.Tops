377ab2448635e6787c3db63c9afcaa779c695c25
rathod-tirth <tirthrathod6@gmail.com>
2024-01-25 18:47:04 +0530

    add notice table

--- Digital_Society/myproject/myapp/models.py
+++ Digital_Society/myproject/myapp/models.py
@@ -42,8 +42,11 @@ class Member(models.Model):
        return f"{self.firstname} | {self.blockno}/{self.houseno}"
 
 class Notice(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
+    authority=models.CharField(max_length=20)
+    notice_title = models.CharField(max_length=50)
+    notice_text = models.TextField()
+    name=models.CharField(max_length=20)
+    designation=models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
