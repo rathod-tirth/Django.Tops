5b3076c4daafbd9c785ebea5d596ac997af25fdd
rathod-tirth <tirthrathod6@gmail.com>
2024-02-20 19:33:57 +0530

    add api views for user

--- Django-API/myproject/myapp/views.py
+++ Django-API/myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class GetUser(generics.ListCreateAPIView):
+   queryset=User.objects.all()
+   serializer_class=UserSerializer
+   
+class ControlUser(generics.RetrieveUpdateDestroyAPIView):
+   queryset=User
+   serializer_class=UserSerializer
