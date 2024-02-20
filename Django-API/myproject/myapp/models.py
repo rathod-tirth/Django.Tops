d48c737a7f68afbc8d162d3cc3ea2662b65668d2
rathod-tirth <tirthrathod6@gmail.com>
2024-02-20 19:33:25 +0530

    add User model

--- Django-API/myproject/myapp/models.py
+++ Django-API/myproject/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+   name=models.CharField(max_length=30)
+   age=models.CharField(max_length=50)   
+   phone=models.CharField(max_length=10)
+   city=models.CharField(max_length=20)
+   state=models.CharField(max_length=20)
+   
+   def __str__(self):
+       return self.name
