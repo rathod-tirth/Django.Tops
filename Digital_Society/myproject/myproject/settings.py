f020c6f48d14ba5060f43d57d38e98c565383bbe
Tirth Rathod <122792938+rathod-tirth@users.noreply.github.com>
2024-05-06 17:11:01 +0530

    Update settings.py

--- Digital_Society/myproject/myproject/settings.py
+++ Digital_Society/myproject/myproject/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tirthrathod6@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
